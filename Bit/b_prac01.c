9f39bb8e5a152f46f68f5542470668940a3a713c
Murata-KIC <152669395+kic-murata@users.noreply.github.com>
2024-07-01 15:09:10 +0900

    20240701

--- Bit/b_prac01.c
+++ Bit/b_prac01.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h> //
+#include<time.h>
 #define Skil_Num 3
 #define Mob_Num 3
 //符号無し整数をUINTで再定義
@@ -22,7 +22,7 @@ typedef struct { //Spec型構造体
 typedef struct { //Chara型構造体
 	Spec sp;       //Spec型構造体変数
 	int maxhp;     //最大HP
-	int mp;        //SP
+	int mp;        //MP
 	Skill skl[Skil_Num]; //Skill型構造体配列
 } Chara;
 typedef struct { //Mob型構造体
@@ -36,30 +36,32 @@ enum BitState
 	//通常状態
 	Base = 0,            //0000 0000 0000 0000
 	//(1からいくつ左にシフトするか)
-	Poison = 1 << 0,     //0000 0000 0000 0001(どく)
-	Sleep = 1 << 1,      //0000 0000 0000 0010(ねむり)
-	Paralysis = 1 << 2,  //0000 0000 0000 0100(まひ)
-	Burn = 1 << 3,       //0000 0000 0000 1000(やけど)
+	Poison = 1 << 0,     //0000 0000 0000 0001(毒)
+	Sleep = 1 << 1,      //0000 0000 0000 0010(睡眠)
+	Paralysis = 1 << 2,  //0000 0000 0000 0100(麻痺)
+	Burn = 1 << 3,       //0000 0000 0000 1000(火傷)
 	AtkUp = 1 << 4,      //0000 0000 0001 0000(攻撃力Up)
 	AtkDown = 1 << 5,    //0000 0000 0010 0000(攻撃力Down)
-	Dead = 1 << 6        //0000 0000 0100 0000(死亡フラグ)
+	Dead = 1 << 6,       //0000 0000 0100 0000(死亡フラグ)
+	Atk_Skill = 1 << 7   //0000 0000 1000 0000
+	         //(このフラグがONのキャラしか状態異常を付加できない）
 };
 
-int TurnCount = 0;       //経過ターン(0～999)
+int TurnCount = 0;     //経過ターン(0～999)
 
 //状態を変更する関数
-void ChangeFlag(UINT *s);
+//void ChangeFlag(UINT *s);
 //状態を回復する関数
-void ClearFlag(UINT *s);
+//void ClearFlag(UINT *s);
 //状態を表示する関数
 void DisplayStatus(Chara c);
 
+//戦闘モードへ移行する関数
+void BattleMode(Chara* c, Mob m);
 //戦闘時のメニュー表示を行う
 int DisplayMenu(void);
 //スキル使用時のメニュー表示を行う
 int SkillMenu(Chara c);
-//戦闘モードへ移行する関数
-void BattleMode(Chara* c, Mob m);
 //攻撃側と防御側のパラメータを使った攻撃ダメージ計算
 int DamageCalc(Spec sp1, Spec sp2);
 //戦闘モード時のHP計算と死亡判定
@@ -78,13 +80,13 @@ main(int argc,char *argv[])
 	Chara chara = { "主人公",2000, 200, 100, Base, 2000, 150, 
     //  sk.name    type usemp effect 
 	   {{"HP回復",     0,   50,  800},
-		  {"AtkUp",     1,   50,  115},
-		  {"状態異常回復",2,   20,    0}} };
-	Mob mob[3] = { 
-		//name hp   atk def  state   rate%
-		{"敵A", 700,150, 800,Poison,  50},
-		{"敵B",1500,200, 200,Burn,    50},
-		{"ボス",5000,200, 80,AtkDown, 10} };
+		  {"攻撃力アップ", 1,   50,  120},
+		  {"状態異常回復", 2,   20,    0}} };
+	Mob mob[Mob_Num] = { 
+		//name hp   atk def  state               rate%
+		{"敵A", 700,150, 800,Poison | Atk_Skill,  30},
+		{"敵B",1500,200, 200,Burn   | Atk_Skill,  30},
+		{"ボス",5000,200, 80,AtkDown | Atk_Skill, 10} };
 
 	//乱数で0~Mob_Num-1の数値を求める
 	int num = rand() % Mob_Num;
@@ -108,11 +110,22 @@ main(int argc,char *argv[])
 * 与ダメ計算関数
 * 第1引数：Spec型構造体変数（攻撃側）
 * 第2引数：Spec型構造体変数（防御側）
-* （与ダメ）＝攻撃側Atk*(攻撃側Atk÷防御側Def）＋攻撃側Atk*乱数0~0.5
-* 未実装項目：会心の一撃（通常の1.5倍ダメ）
+* （与ダメ）＝攻撃側Atk×(攻撃側Atk÷防御側Def）＋攻撃側Atk×乱数0~0.5
 */
 int DamageCalc(Spec sp1, Spec sp2) {
-	return sp1.atk * ((float)sp1.atk / sp2.def) + sp1.atk * (rand() % 50) / 100.0;
+	int damage;
+	//攻撃力ダウンフラグがONで、Atk_SkillがOFFのとき（主人公のみこの条件に該当）
+	if ((sp1.state & AtkDown) && !(sp1.state & Atk_Skill)) {
+		//攻撃力を0.8倍にしてダメージ計算
+		sp1.atk *= 0.8;
+	}
+	damage = sp1.atk * ((float)sp1.atk / sp2.def) 
+		       + sp1.atk * (rand() % 50) / 100.0;
+	//100分の1の確率で会心/痛恨の一撃
+	if (rand() % 100 == 99) {
+		return 2 * damage;  //ダメージ量2倍
+	}
+	return damage;  //通常ダメージ
 }
 /*void BattleMessage(Spec sp1, Spec* sp2)
 * 戦闘モード時のメッセージ表示を行う
@@ -134,10 +147,11 @@ void BattleMessage(Spec sp1, Spec* sp2) {
 /*void BattleMode(Chara* c, Mob m)
 * 戦闘モード時のメッセージ表示を行う
 * 第1引数：Chara型構造体変数のアドレス（自キャラ）
-* 第2引数：Mob型構造体変数（敵キャラ）*/
+* 第2引数：Mob型構造体変数（敵キャラ）
+*/
 void BattleMode(Chara* c, Mob m) {
 	int command, skill;
-	system("cls");
+	system("cls");      //コマンドプロンプトのcls命令の実行
 	DisplayStatus(*c);
 	printf("%sがあらわれた！\n", m.sp.name);
 	while (1) {
@@ -145,31 +159,31 @@ void BattleMode(Chara* c, Mob m) {
 		if (c->sp.state & Poison) {
 			c->sp.hp *= 0.8;
 		}
-		if (c->sp.state & AtkDown) {
-			c->sp.atk *= 0.95;
-		}
 		if (c->sp.state & Burn) {
 			c->sp.hp *= 0.9;
 			c->mp *= 0.9;
 		}
-
-
+		//[たたかう]
 		if (command == 1) {
 			DisplayStatus(*c);
+			//TurnCountが偶数のときは自キャラの攻撃
 			if (TurnCount % 2 == 0) {
 				BattleMessage(c->sp, &m.sp);
-
+				//敵キャラの死亡フラグがONなら
 				if (m.sp.state & Dead) {
 					break;
 				}
 			}
+			//TurnCountが奇数のときは敵の攻撃
 			if (TurnCount % 2 == 1) {
 				BattleMessage(m.sp, &c->sp);
+				//状態異常付加確率の計算
 				if (rand() % 100 < m.rate) {
 					printf("状態異常攻撃を受けた！\n");
-					c->sp.state |= m.sp.state;
+					//主人公に状態異常を付加するときには、Atk_Skillフラグを除去しておく
+					c->sp.state |= (m.sp.state & ~Atk_Skill);
 				}
-
+				//自キャラの死亡フラグがONなら
 				if (c->sp.state & Dead) {
 					break;
 				}
@@ -178,6 +192,7 @@ void BattleMode(Chara* c, Mob m) {
 				}
 			}
 		}
+		//[スキル]
 		else if (command == 2) {
 			printf("スキルの選択\n");
 			skill = SkillMenu(*c);
@@ -199,7 +214,27 @@ void BattleMode(Chara* c, Mob m) {
 					c->sp.atk *= c->skl[skill].effect / 100.0;
 					//MPを減らす
 					c->mp -= c->skl[skill].use_mp;
+					//攻撃力アップフラグON
+					c->sp.state |= AtkUp;
+					DisplayStatus(*c);
 					printf("攻撃力がアップした!\n");
+					break;
+				case 2:
+					//攻撃力アップは異常ではないので残す
+					if (c->sp.state & AtkUp) {
+						//状態変化をリセット
+						c->sp.state = Base;
+						//攻撃力アップを再度付加
+						c->sp.state |= AtkUp;
+					}
+					else {
+						//状態変化をリセット
+						c->sp.state = Base;
+					}
+					//MPを減らす
+					c->mp -= c->skl[skill].use_mp;
+					DisplayStatus(*c);
+					printf("状態異常が回復した!\n");
 					break;
 				default:
 					break;
@@ -208,7 +243,6 @@ void BattleMode(Chara* c, Mob m) {
 			else {
 				printf("MPが足りない！\n");
 			}
-
 		}
 		//強制終了モード選択時の処理
 		else if (command == -1) {
@@ -219,7 +253,8 @@ void BattleMode(Chara* c, Mob m) {
 }
 /*int DisplayMenu(void)
 * 戦闘モード時のコマンド選択画面を表示
-* 戻り値：選択したコマンドのメニュー番号 */
+* 戻り値：選択したコマンドのメニュー番号(-1のみ強制終了）
+*/
 int DisplayMenu(void) {
 	char ch;
 	while (1) {
